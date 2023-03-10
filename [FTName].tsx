b08a0d421db7f341106ce4d27637f2fe6a0cf52f
a2d7cc <a2d7cc@gmail.com>
2023-03-10 11:22:27 +0100

    Template generate structure for React Components

---
+++ [FTName].tsx
@@ -0,0 +1,10 @@
+import {FC} from 'react';
+import styles from './[FTName].module.scss'
+import {I<FTName>} from './[FTName].interface'
+
+const <FTName>: FC<I<FTName>> = () => {
+  return <div className={styles.<FTName | lowercase
+>}></div>
+}
+
+export default <FTName>
